0dfd75c96f0f4fa179b78eaa8397d2db2207c50c
Fl2on <69091361+fl2on@users.noreply.github.com>
2026-03-27 16:06:49 -0500

    Update admin email in test setup

--- Charla #2/auth/src/test/setup.ts
+++ Charla #2/auth/src/test/setup.ts
@@ -10,7 +10,7 @@ declare global {
 let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = "asdfasdf";
-  process.env.ADMIN_EMAIL = "fl2on@proton.me";
+  process.env.ADMIN_EMAIL = "test@test.me";
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
   mongo = await MongoMemoryServer.create();
